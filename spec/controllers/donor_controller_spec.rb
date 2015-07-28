1968cca6334c632b6803537fe3d0c0eb7e2d79a0
Kiloreux <kiloreux@gmail.com>
2015-07-28 18:44:58 +0100

    Added more responsiveness it seems i did not check the materialize documentation enough

---
+++ spec/controllers/donor_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DonorController, type: :controller do
+
+end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  it "renders the index template" do
+    get :index
+    expect(response).to render_template("index")
+  end
+
+end
