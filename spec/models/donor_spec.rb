0b8a49a41f5f442ac5d5d2c0c43861a30e9a7eff
Kiloreux <kiloreux@gmail.com>
2015-08-29 07:33:19 +0100

    Removed deprecated Rails tests and moved to expect instead of should

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Donor, type: :model do
   it "has a valid factory" do
-    FactoryGirl.create(:donor).should be_valid
+    expect(FactoryGirl.create(:donor)).to be_valid
   end
 
   it "is not valid without a city" do
-    FactoryGirl.build(:donor,city: nil).should_not be_valid
+    expect(FactoryGirl.build(:donor,city: nil)).to_not be_valid
   end
 
   it "is not valid without a name" do
-    FactoryGirl.build(:donor,name: nil).should_not be_valid
+    expect(FactoryGirl.build(:donor,name: nil)).to_not be_valid
   end
 
   it "is not valid without  a phone number" do
-    FactoryGirl.build(:donor,phone_number: nil).should_not be_valid
+    expect(FactoryGirl.build(:donor,phone_number: nil)).to_not be_valid
   end
 
   it "is not valid without a blood type" do
-    FactoryGirl.build(:donor,blood_type: nil).should_not be_valid
+    expect(FactoryGirl.build(:donor,blood_type: nil)).to_not be_valid
   end
 
 end
