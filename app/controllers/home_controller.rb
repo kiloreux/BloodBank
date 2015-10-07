c4691fc8a056dac7d9aec7230aeb759c9e171c8d
kiloreux <kiloreux@gmail.com>
2015-10-08 00:37:30 +0100

    Added counting donors by blood stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @donor=Donor.new
+    @donors=Donor.all
     if session[:errors]
       session[:errors].each {|error, error_message| @donor.errors.add error, error_message}
       session.delete :errors
